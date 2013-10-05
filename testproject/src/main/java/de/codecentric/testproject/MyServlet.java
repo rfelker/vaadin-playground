3ec9af733b6183feed43b58c45ac5b5d35953f94
Rolf Felker <rolf.felker@codecentric.de>
2013-10-05 11:38:56 +0200

    add BootstrapListener

--- testproject/src/main/java/de/codecentric/testproject/MyServlet.java
+++ testproject/src/main/java/de/codecentric/testproject/MyServlet.java
@@ -6,6 +6,9 @@ import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 
 import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.BootstrapFragmentResponse;
+import com.vaadin.server.BootstrapListener;
+import com.vaadin.server.BootstrapPageResponse;
 import com.vaadin.server.ServiceException;
 import com.vaadin.server.SessionDestroyEvent;
 import com.vaadin.server.SessionDestroyListener;
@@ -14,12 +17,12 @@ import com.vaadin.server.SessionInitListener;
 import com.vaadin.server.VaadinServlet;
 
 @WebServlet(value = "/*", asyncSupported = true)
-@VaadinServletConfiguration(productionMode = false, ui = MyVaadinUI.class, widgetset = "de.codecentric.testproject.AppWidgetSet")
+@VaadinServletConfiguration(heartbeatInterval = 3, productionMode = false, ui = MyVaadinUI.class, widgetset = "de.codecentric.testproject.AppWidgetSet")
 public class MyServlet extends VaadinServlet implements SessionInitListener,
-		SessionDestroyListener {
+		SessionDestroyListener, BootstrapListener {
 
-	private static final Logger LOGGER = Logger.getLogger(MyServlet.class
-			.getName());
+	private static final long serialVersionUID = -2814562561177596808L;
+	private static final Logger LOGGER = Logger.getLogger(MyServlet.class.getName());
 
 	@Override
 	protected void servletInitialized() throws ServletException {
@@ -38,5 +41,18 @@ public class MyServlet extends VaadinServlet implements SessionInitListener,
 	@Override
 	public void sessionInit(SessionInitEvent event) throws ServiceException {
 		LOGGER.info("session init, event=" + event);
+
+		event.getSession().addBootstrapListener(this);
+	}
+
+	@Override
+	public void modifyBootstrapFragment(BootstrapFragmentResponse response) {
+		LOGGER.info("modifyBootstrapFragment, event=" + response);
+	}
+
+	@Override
+	public void modifyBootstrapPage(BootstrapPageResponse response) {
+		LOGGER.info("modifyBootstrapPage, event=" + response);
+		LOGGER.info("modifyBootstrapPage, document=" + response.getDocument());
 	}
 }

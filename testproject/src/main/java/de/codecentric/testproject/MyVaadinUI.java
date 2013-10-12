176ad4338990e92ebe00040a377e3695a490bc5c
Rolf Felker <rolf.felker@codecentric.de>
2013-10-12 17:11:05 +0200

    create a default layout

--- testproject/src/main/java/de/codecentric/testproject/MyVaadinUI.java
+++ testproject/src/main/java/de/codecentric/testproject/MyVaadinUI.java
@@ -1,18 +1,23 @@
 package de.codecentric.testproject;
 
-import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 import com.vaadin.annotations.PreserveOnRefresh;
 import com.vaadin.annotations.Theme;
 import com.vaadin.server.ClientConnector.AttachListener;
 import com.vaadin.server.ClientConnector.DetachListener;
-import com.vaadin.server.Page;
+import com.vaadin.server.ExternalResource;
 import com.vaadin.server.VaadinRequest;
-import com.vaadin.server.VaadinService;
+import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Link;
+import com.vaadin.ui.TabSheet;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
@@ -20,26 +25,59 @@ import com.vaadin.ui.VerticalLayout;
 @SuppressWarnings("serial")
 @PreserveOnRefresh
 public class MyVaadinUI extends UI implements DetachListener, AttachListener {
-	private static final Logger LOGGER = Logger.getLogger(MyVaadinUI.class.getName());
+	private static final int TAB_COUNT = 8;
+	final private static Logger LOGGER = Logger.getLogger(MyVaadinUI.class.getName());
+	final private List<VerticalLayout> tabList = new ArrayList<VerticalLayout>();;
 
 	@Override
 	protected void init(VaadinRequest request) {
 		addDetachListener(this);
 		addAttachListener(this);
 
-		final VerticalLayout layout = new VerticalLayout();
-		layout.setMargin(true);
-		layout.setSizeFull();
-		layout.setCaption("my caption");
-		setContent(layout);
+		final HorizontalLayout rootLayout = new HorizontalLayout();
+		rootLayout.setMargin(false);
+		rootLayout.setSizeFull();
+		setContent(rootLayout);
 
-		Button button = new Button("Click Me");
-		button.addClickListener(new Button.ClickListener() {
-			public void buttonClick(ClickEvent event) {
-				layout.addComponent(new Label("Thank you for clicking"));
-			}
-		});
-		layout.addComponent(button);
+		final VerticalLayout leftMenu = new VerticalLayout();
+		createDummyLeftMenuEntries(leftMenu);
+		leftMenu.setWidth(200, Unit.PIXELS);
+		leftMenu.setHeight(100, Unit.PERCENTAGE);
+		leftMenu.setMargin(false);
+		rootLayout.addComponent(leftMenu);
+
+		final VerticalLayout rightArea = new VerticalLayout();
+		rightArea.setMargin(false);
+		rightArea.setSizeFull();
+		rootLayout.addComponent(rightArea);
+		rootLayout.setExpandRatio(rightArea, 1);
+
+		TabSheet tabSheet = new TabSheet();
+		createDummyTabs(tabSheet);
+		// tabSheet.setSizeFull();
+
+		rightArea.addComponent(tabSheet);
+		rightArea.setExpandRatio(tabSheet, 1);
+		rightArea.addComponent(createFooter());
+
+	}
+
+	private void createDummyLeftMenuEntries(final VerticalLayout leftMenu) {
+
+		for (int i = 0; i < TAB_COUNT; i++) {
+
+			Button button = new Button("Click Me " + (i + 1));
+			button.setData(i);
+
+			button.addClickListener(new Button.ClickListener() {
+				public void buttonClick(ClickEvent event) {
+					getTabList().get((Integer) event.getButton().getData()).addComponent(
+							new Label("Thank you for clicking"));
+				}
+			});
+			leftMenu.addComponent(button);
+			leftMenu.setComponentAlignment(button, Alignment.MIDDLE_CENTER);
+		}
 
 		Button logout = new Button("Logout");
 		logout.addClickListener(new Button.ClickListener() {
@@ -49,17 +87,48 @@ public class MyVaadinUI extends UI implements DetachListener, AttachListener {
 				getSession().close();
 			}
 		});
-		layout.addComponent(logout);
+		leftMenu.addComponent(logout);
+		leftMenu.setComponentAlignment(logout, Alignment.MIDDLE_CENTER);
+	}
 
-		Page page = Page.getCurrent();
-
-		File baseDirectory = VaadinService.getCurrent().getBaseDirectory();
-
-		if (baseDirectory != null) {
-			LOGGER.info("baseDir=" + baseDirectory.getAbsolutePath());
-		} else {
-			LOGGER.info("baseDir=null");
+	private void createDummyTabs(TabSheet tabSheet) {
+		for (int i = 0; i < TAB_COUNT; i++) {
+			final VerticalLayout tabLayout = new VerticalLayout(new Label("Content " + (i + 1)));
+			getTabList().add(tabLayout);
+			tabLayout.setMargin(false);
+			tabLayout.setSizeFull();
+			tabSheet.addTab(tabLayout, "Tab " + (i + 1));
 		}
+	}
+
+	private Component createFooter() {
+		HorizontalLayout footerLayout = new HorizontalLayout();
+		footerLayout.setMargin(false);
+		footerLayout.setSizeFull();
+		footerLayout.setHeight(30, Unit.PIXELS);
+
+		HorizontalLayout linkLayout = new HorizontalLayout();
+		linkLayout.setSizeFull();
+		linkLayout.setMargin(false);
+		linkLayout.setSpacing(true);
+
+		footerLayout.addComponent(new Link()); // dummy
+		footerLayout.addComponent(linkLayout);
+		footerLayout.addComponent(new Link());// dummy
+
+		ExternalResource resource = new ExternalResource("http://www.codecentric.de");
+		Link contactLink = new Link("Contact", resource);
+		Link aboutLink = new Link("About us", resource);
+		Link couLink = new Link("Conditions of Use", resource);
+
+		linkLayout.addComponent(contactLink);
+		linkLayout.setComponentAlignment(contactLink, Alignment.MIDDLE_CENTER);
+		linkLayout.addComponent(aboutLink);
+		linkLayout.setComponentAlignment(aboutLink, Alignment.MIDDLE_CENTER);
+		linkLayout.addComponent(couLink);
+		linkLayout.setComponentAlignment(couLink, Alignment.MIDDLE_CENTER);
+
+		return footerLayout;
 	}
 
 	@Override
@@ -71,5 +140,9 @@ public class MyVaadinUI extends UI implements DetachListener, AttachListener {
 	public void attach(AttachEvent event) {
 		LOGGER.info("UI attach, event=" + event);
 
+	}
+
+	public List<VerticalLayout> getTabList() {
+		return tabList;
 	}
 }
